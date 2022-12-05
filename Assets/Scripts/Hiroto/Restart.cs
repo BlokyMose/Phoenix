e9974b842505aaf45f74953268c0f47caaf749dd
HirotoKanno <a20dc060@dhw.ac.jp>
2022-12-06 03:17:14 +0900

    add

---
+++ Assets/Scripts/Hiroto/Restart.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Phoenix
+{
+    public class Restart : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void Retry()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
