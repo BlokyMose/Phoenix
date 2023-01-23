cd73704cc16eafae0436862027d82a8344630fec
Leafa0419 <hiroto-k0413@outlook.jp>
2023-01-24 02:43:28 +0900

    UI

---
+++ Assets/Scripts/Hiroto/UI/Stage2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+    public class Stage2 : MonoBehaviour
+    {
+       
+            public void OnClickStartButton()
+            {
+                SceneManager.LoadScene("Level_Phoenix_2");
+            }
+        
+    }
+
